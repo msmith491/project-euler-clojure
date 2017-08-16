8e808153cfae8bc61398d8af24e2f10e5d1c4ecf
Matt Smith <mss@datera.io>
2017-08-16 17:24:18 -0500

    Updating core.clj so we're dynamically loading requested problem
    Also added profiling for the requested problem

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -1,13 +1,20 @@
 (ns project-euler.core
   (:gen-class :main true))
-(load "problem_1")
-(load "problem_2")
-(load "problem_3")
+
+(loop
+  [x 4]
+  (load (format "problem_%s" x))
+  (cond
+    (< 1 x) (recur (dec x))
+    :else nil))
 
 (defn -main
   "Project Euler Problems In Clojure"
   [& args]
+  (def t1 (.getTime (java.util.Date.)))
   (if (= (count args) 0)
     (System/exit 0)
-      ((resolve
-          (symbol "project-euler.core" (first args))))))
+    ((resolve
+        (symbol "project-euler.core" (first args)))))
+  (def t2 (.getTime (java.util.Date.)))
+  (println "Time: " (float (/ (- t2 t1) 1000))  , "s"))
